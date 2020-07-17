a19d69cf6f84653976f456c997774c5f372a5222
AFSUCDAVIS <afsucdavis@gmail.com>
2020-07-17 10:01:12 -0700

    try

---
+++ story_content/6mUMQIyxYzL_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.41.22450.0%0D%0A%0D%0A00:00:00.128%20--%3E%2000:00:04.728%0D%0AWhen%20driving%20with%20snow%20chains%20on%20the%20highway,%20remember%20that%20the%20speed%20limit%20is%20always%2030mph.%0D%0A%0D%0A"}')
